2f1ab66b568107be04f066c5140ce14f995c5cd4
Frank <ffavela@gmail.com>
2025-10-29 20:31:03 -0600

    first implementation of the whole numbers

---
+++ wholes.py
@@ -0,0 +1,200 @@
+class Wholes:
+    """Something that contains the Whole numbers"""
+    def __init__(self, start=0, stop=None, step=1,
+                 raw_repr=False):
+        if start is None:
+            start = 0
+        if not isinstance(step, int):
+            raise ValueError('Values need to be integers.')
+        if stop is not None:
+            if not isinstance(stop, int):
+                raise ValueError('Values need to be integers.')
+        if start < 0 and stop is None:
+            raise ValueError('No last value exists.')
+        if stop is not None:
+            if stop < start and step > 0 \
+               or stop > start and step < 0:
+                raise ValueError('Invalid initialization state.')
+        if start == 0 and stop is None and step == 1:
+            sliced = False
+        else:
+            sliced = True
+
+        self.start = start
+        self.step = step
+        self.stop = stop
+        self.sliced = sliced
+        self.raw_repr = raw_repr
+
+    def step_function(self, i: int) -> int:
+        """A simple step function"""
+        if i == 0:
+            return 0
+        return 1
+
+    def __len__(self):
+        if self.stop is not None:
+            delta = abs(self.stop - self.start)
+            return self.step_function(delta % self.step) +\
+                delta // abs(self.step)
+        raise ValueError('Aleph_0 infinite')
+
+    def __getitem__(self, key):
+        enum_error = 'Cannot enumerate backward from infinite'
+        if isinstance(key, slice):
+            kstep = key.step
+            if key.step is None:
+                step = self.step
+                kstep = 1
+            else:
+                step = self.step * key.step
+
+            flip = True if step * self.step < 0 else False
+
+            if key.stop is None:
+                s_stop = self.stop
+            else:
+                kstop = key.stop
+                if kstop < 0:
+                    if self.stop is None:
+                        raise ValueError(enum_error)
+                    if self.__len__() < -kstop:  # cause kstop < 0
+                        kstop = -1
+                    else:
+                        # The following satisfies:
+                        # 0 < kstop < self.__len__()
+                        kstop += self.__len__()
+                # The next satisfies kstop > 0 too
+                elif flip and kstop >= self.__len__():
+                    kstop = self.__len__() - 1
+                s_stop = self.start + kstop * self.step
+
+            if s_stop is None and step < 0:
+                raise ValueError(enum_error)
+
+            if key.start is None:
+                s_start = self.start
+            else:
+                kstart = key.start
+                if kstart < 0:
+                    if s_stop is None:
+                        raise ValueError(enum_error)
+                    if self.__len__() < -kstart:  # cause kstart < 0
+                        kstart = -1
+                    else:
+                        # The following satisfies:
+                        # 0 < kstart < self.__len__()
+                        kstart += self.__len__()
+                # The next satisfies kstart > 0 too
+                elif flip and kstart > self.__len__():
+                    kstart = self.__len__() - 1
+                # If despite of trying it is still negative
+                if not flip and kstart < 0:
+                    kstart = 0
+                s_start = self.start + kstart * self.step
+
+            if flip and step < 0:
+                if key.stop is None:
+                    s_stop = self.start - self.step
+                if key.start is None:
+                    if self.stop is None:
+                        s_start = s_stop - self.step
+                    else:
+                        s_start = self.stop - self.step
+
+            if s_stop is None:
+                start = s_start
+                stop = s_stop
+            else:
+                if (s_stop < s_start and step > 0
+                   or s_stop > s_start and step < 0)\
+                   and (key.start is not None or
+                        key.stop is not None):
+                    delta = 0
+                else:
+                    delta = abs(s_stop - s_start)
+
+                len_raw = delta // abs(self.step) +\
+                    self.step_function(delta % self.step)
+                len_step = len_raw // abs(kstep) +\
+                    self.step_function(len_raw % kstep)
+
+                start = s_start
+                stop = start + len_step * step
+
+            if flip:
+                if self.step < 0:  # step > 0
+                    start, stop = s_stop-self.step, s_start-self.step
+
+            return Wholes(start, stop, step, self.raw_repr)
+
+        if isinstance(key, int):
+            i = int(key)
+            if i < 0:
+                if self.stop is None:
+                    msg = 'Cannot count backward from an Aleph_0 infinite set'
+                    raise IndexError(msg)
+                if i + self.__len__() < 0:
+                    raise IndexError('eset index out of range')
+                i = self.__len__() + i
+            if i >= 0:
+                if self.stop is None or i < self.__len__():
+                    return (self.start + i * self.step)
+                raise IndexError('eset index out of range')
+        raise ValueError('Need a slice or a positive integer')
+
+    def __contains__(self, val):
+        diff = val - self.start
+        if diff % self.step != 0:
+            return False
+        if self.step > 0 and val >= self.start:
+            if self.stop is None or\
+               val < self.stop:
+                return True
+        if self.step < 0 and self.stop < val <= self.start:
+            return True
+        return False
+
+    def index(self, val):
+        if val not in self:
+            cls = type(self)
+            msg = f'{val!r} not in {cls.__name__!r}'
+            raise ValueError(msg)
+        return (val - self.start)//(self.step)
+
+    def iter_condition(self, i):
+        if self.stop is None:
+            return True
+        else:
+            return i < self.__len__()
+
+    def __iter__(self, i=0):
+        while self.iter_condition(i):
+            yield self.start + i * self.step
+            i += 1
+
+    def __repr__(self):
+        max_val = 4
+        if not self.raw_repr:
+            ellipsis = ', ...'
+            try:
+                last = min(max_val, len(self))
+                if max_val >= len(self):
+                    ellipsis = ''
+            except ValueError:
+                last = max_val
+            rstr = ', '.join([str(v) for v in self[:last]])
+            rstr += ellipsis
+            sliceStr = '*' if self.sliced else ''
+            return '<esets.Wholes'+sliceStr+f' ({rstr})>'
+        else:
+            return f'self.start = {self.start},\n' +\
+                f'self.stop = {self.stop},\n' +\
+                f'self.step = {self.step},\n' +\
+                f'self.sliced = {self.sliced},\n' +\
+                f'self.raw_repr = {self.raw_repr}'
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testfile("docTest.txt")
