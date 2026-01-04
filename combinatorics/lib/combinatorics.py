d52ecd0a713ab4f27401ac00c63d75813ce1b5af
Frank <ffavela@gmail.com>
2026-01-03 21:54:42 -0600

    first try for permutations

---
+++ combinatorics/lib/combinatorics.py
@@ -0,0 +1,24 @@
+from math import factorial
+
+
+def get_permutation(val: int, size: int) -> tuple[int] | None:
+    factorial_n = factorial(size)
+
+    if val >= factorial_n or val < 0:
+        return None
+
+    def get_idx_list(resval: int, reslist: list[int],
+                     retlist: list[int]):
+        if len(reslist) == 1:
+            retlist.append(reslist[0])
+            return retlist
+
+        fact_val = factorial(len(reslist)-1)
+        div, mod = divmod(resval, fact_val)
+
+        val = reslist.pop(div)
+        retlist.append(val)
+        return get_idx_list(mod, reslist, retlist)
+
+    reslist = list(range(size))
+    return tuple(get_idx_list(val, reslist, []))
