300b0cedf324978c9198c36150b2d0e39ec149a6
Frank <ffavela@gmail.com>
2026-01-16 15:09:17 -0600

    simple contains for integers

--- esets.py
+++ esets.py
@@ -77,41 +77,7 @@ class Integers(Eset):
     def __contains__(self, val):
         if not isinstance(val, int):
             return False
-        # simple_contains is too simple for this case
-        diff = val - self.direct_function(self.start)
-        if diff % self.step != 0:
-            return False
-        if self.step > 0:
-            if abs(val) > abs(self.direct_function(self.start)):
-                if self.stop is None:
-                    return True
-                if abs(val) < abs(self.direct_function(self.stop)):
-                    return True
-                else:  # the abs == case for stop
-                    if val < self.direct_function(self.stop):
-                        return False
-                    else:
-                        return True
-            if abs(val) == abs(self.direct_function(self.start)):
-                if val > self.direct_function(self.start):
-                    return False
-                else:
-                    return True
-        if self.step < 0:
-            if abs(self.direct_function(self.stop)) <\
-               abs(val) < abs(self.direct_function(self.start)):
-                return True
-            if abs(self.direct_function(self.stop)) == abs(val):
-                if val < self.direct_function(self.stop):
-                    return True
-                else:
-                    return False
-            if abs(val) == abs(self.direct_function(self.start)):
-                if val < self.direct_function(self.start):
-                    return False
-                else:
-                    return True
-        return False
+        return self.simple_contains(val)
 
     def inverse_fun(self, val):
         if val > 0:
