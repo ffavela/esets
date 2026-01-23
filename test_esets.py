8dbeaae595567ebb3e43c7882aebb932395e91c9
Frank <ffavela@gmail.com>
2026-01-22 22:38:42 -0600

    pytest raises stuff an wholes wip

--- test_esets.py
+++ test_esets.py
@@ -1,8 +1,9 @@
-from esets import Evens
+import esets
+import pytest
 
 
 def test_evens():
-    e = Evens()
+    e = esets.Evens()
     assert 8 in e
     assert 2**(10**5) in e
     assert 5 not in e
@@ -13,3 +14,13 @@ def test_evens():
     assert e[1] == 2
     assert e[10] == 20
     assert e[617171621621171170] == 1234343243242342340
+    with pytest.raises(ValueError):
+        len(e)
+    with pytest.raises(ValueError, match='Aleph_0 infinite'):
+        len(e)
+
+
+def test_wholes():
+    w = esets.Wholes()
+    assert 42 in w
+    assert 3.14 not in w
